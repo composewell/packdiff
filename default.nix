222fda8e211ac07d3d9185af4c26bbc8e448f975
Adithya Kumar <adithya@composewell.com>
2022-09-23 11:35:17 +0530

    Add a default.nix and move cabal.project to cabal.project.user

---
+++ default.nix
@@ -0,0 +1,46 @@
+# CAUTION! a spelling mistake in arg string is ignored silently.
+#
+# To use ghc-8.10.7
+# nix-shell --argstr compiler "ghc8107"
+
+{
+  nixpkgs ?
+    import (builtins.fetchTarball
+      https://github.com/NixOS/nixpkgs/archive/refs/tags/22.05.tar.gz)
+        {}
+, compiler ? "ghc922"
+}:
+let
+    utils =
+      let src = fetchGit {
+            url = "git@github.com:composewell/composewell-haskell.git";
+            ref = "master";
+          }; in (import "${src}/utils.nix") { inherit nixpkgs; };
+
+
+    haskellPackages =
+      let src = fetchGit {
+            url = "git@github.com:composewell/composewell-haskell.git";
+            ref = "master";
+          }; in (import "${src}/haskellPackages.nix")
+            { inherit nixpkgs;
+              inherit compiler; };
+
+    mkHaskellPackages = inShell:
+      haskellPackages.override (old: {
+        overrides =
+          nixpkgs.lib.composeExtensions
+            (old.overrides or (_: _: {}))
+            (with nixpkgs.haskell.lib; self: super: {
+                packdiff =
+                  utils.local super "packdiff" ./. "" inShell;
+            });
+      });
+
+    shellDrv = mkHaskellPackages true;
+
+    shell = utils.mkShell shellDrv (p: [p.packdiff]) true;
+
+in if nixpkgs.lib.inNixShell
+   then shell
+   else (mkHaskellPackages false).packdiff
